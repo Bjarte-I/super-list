21754e7521a0b00cb784b099cf2552d7acd2fe14
Bjarte Iversen <bjartewi@uia.no>
2021-03-30 20:58:24 +0200

    Added tests for TodoListHandler and the util class.

--- app/src/androidTest/java/com/example/superlist/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/superlist/ExampleInstrumentedTest.kt
@@ -2,11 +2,15 @@ package com.example.superlist
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.superlist.models.Todo
+import com.example.superlist.models.TodoList
+import com.example.superlist.util.getPickedTodoListIndex
+import com.google.common.truth.Truth.assertThat
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
+
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -19,6 +23,86 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.superlist", appContext.packageName)
+        assertThat("com.example.superlist").isEqualTo(appContext.packageName)
+    }
+
+    @Test
+    fun addTodoList_isCorrect() {
+        val myTodoList = TodoList("Numbers", mutableListOf(Todo("1", true), Todo("2", false)))
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        TodoListManager.instance.apply {
+            load()
+            addTodoList(myTodoList, appContext)
+        }
+        assertThat(TodoListManager.instance.getCollection()).contains(myTodoList)
+    }
+
+    @Test
+    fun removeTodoList_isCorrect() {
+        val myTodoList = TodoList("Numbers", mutableListOf(Todo("1", true), Todo("2", false)))
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        TodoListManager.instance.apply {
+            load()
+            if(!(getCollection().contains(myTodoList))){
+                addTodoList(myTodoList, appContext)
+            }
+            removeTodoList(myTodoList, appContext)
+        }
+        assertThat(TodoListManager.instance.getCollection()).doesNotContain(myTodoList)
+    }
+
+    @Test
+    fun addTodo_isCorrect() {
+        val myTodoList = TodoList("Numbers", mutableListOf())
+        val myTodo = Todo("5", true)
+        val listItemIndex:Int
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        TodoListManager.instance.apply {
+            load()
+            if(!(getCollection().contains(myTodoList))) {
+                addTodoList(myTodoList, appContext)
+            }
+            TodoListHolder.PickedTodoList = myTodoList
+            addTodo(myTodo, appContext)
+            listItemIndex = getPickedTodoListIndex()
+        }
+        assertThat(TodoListManager.instance.getCollection()[listItemIndex].listOfTodos).contains(myTodo)
+    }
+
+    @Test
+    fun removeTodo_isCorrect() {
+        val myTodo = Todo("5", true)
+        val myTodoList = TodoList("Numbers", mutableListOf(myTodo))
+        val listItemIndex:Int
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        TodoListManager.instance.apply {
+            load()
+            if(!(getCollection().contains(myTodoList))) {
+                addTodoList(myTodoList, appContext)
+            }
+            TodoListHolder.PickedTodoList = myTodoList
+            listItemIndex = getPickedTodoListIndex()
+            removeTodo(myTodo, appContext)
+        }
+        assertThat(TodoListManager.instance.getCollection()[listItemIndex].listOfTodos).doesNotContain(myTodo)
+    }
+
+    @Test
+    fun updateTodo_isCorrect() {
+        val myTodo = Todo("5", true)
+        val expected = Todo("5", false)
+        val myTodoList = TodoList("Numbers", mutableListOf(myTodo))
+        val listItemIndex:Int
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        TodoListManager.instance.apply {
+            load()
+            if(!(getCollection().contains(myTodoList))) {
+                addTodoList(myTodoList, appContext)
+            }
+            TodoListHolder.PickedTodoList = myTodoList
+            listItemIndex = getPickedTodoListIndex()
+            updateTodo(myTodo, appContext)
+        }
+        assertThat(TodoListManager.instance.getCollection()[listItemIndex].listOfTodos).contains(expected)
     }
 }
